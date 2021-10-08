986901ab076636a39fe3729efbf4c8f938a914a7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 15:10:39 +0300

    Rokas

--- App.js
+++ App.js
@@ -3,18 +3,14 @@ import React from 'react';
 import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import TinderCard from "./src/components/TinderCard";
+import users from './assets/data/users.js';
 
-const jeff = {
-    name: 'Jeff',
-    bio: 'I am jeff',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg'
-}
 
 
 const App = () => {
   return (
     <View style={tw`flex-1 items-center justify-center`}>
-        <TinderCard user={jeff} />
+        <TinderCard user={users[0]} />
       <StatusBar style="auto" />
     </View>
   );
