5897f262d0894b3eac104dfd510e9e9714a3feea
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 13:29:01 +0300

    Rokas

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ import tw from 'tailwind-react-native-classnames';
 const App = () => {
   return (
     <View style={tw`flex-1 items-center justify-center`}>
-        <View style={tw`flex-1 items-center justify-center`}>
-        <Image
-            style={styles.image}
-            source={{uri: 'https://wallpaperaccess.com/full/2582345.jpg'}} />
+        <View style={{width: '95%', backgroundColor: 'orange', height: '70%', borderRadius: 10,}}>
+            <View style={tw`flex-1 items-center justify-center`}>
+                <Image
+                    style={styles.image}
+                    source={{uri: 'https://wallpaperaccess.com/full/2582345.jpg'}} />
+            </View>
         </View>
       <StatusBar style="auto" />
     </View>
@@ -19,8 +21,9 @@ const App = () => {
 
 const styles = StyleSheet.create({
     image: {
-        width: 250,
-        height: 250
+        width: '100%',
+        height: "100%",
+        borderRadius: 10,
     }
 });
 
