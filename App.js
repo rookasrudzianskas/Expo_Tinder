4cc28441fa8147d840f4eb099783428cb39da777
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 13:34:36 +0300

    Rokas

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: "100%",
         borderRadius: 10,
+        overflow: 'hidden',
     },
      card: {
          width: '95%',
