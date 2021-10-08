e13f5844bd374c4d5616bd95901805d27a2a194e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 14:13:42 +0300

    Rokas

--- App.js
+++ App.js
@@ -7,18 +7,7 @@ import tw from 'tailwind-react-native-classnames';
 const App = () => {
   return (
     <View style={tw`flex-1 items-center justify-center`}>
-        <View style={styles.card}>
-            <View style={tw`flex-1 items-center justify-center`}>
-                <ImageBackground
-                    style={styles.image}
-                    source={{uri: 'https://wallpaperaccess.com/full/2582345.jpg'}} >
-                    <View style={tw`p-6`}>
-                        <Text style={{fontSize: 30, color: 'white', fontWeight: 'bold',}}>Rokas R.</Text>
-                        <Text style={{fontSize: 18, color: 'white', lineHeight: 25,}}>Creative fullstack developer. Enthusiastic Growth Hacker, Internet Marketer, and Adobe Creative Cloud Junky</Text>
-                    </View>
-                </ImageBackground>
-            </View>
-        </View>
+
       <StatusBar style="auto" />
     </View>
   );
