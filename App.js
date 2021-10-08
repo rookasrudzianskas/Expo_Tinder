2be44e90f847730302441dbb13f94a87ede28fe7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 12:50:23 +0300

    Rokas

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
-    <View style={tw`flex-1`}>
+    <View style={tw`flex-1 items-center mt-10 justify-center`}>
       <Text style={tw`text-2xl font-bold`}>Tinder 🚀</Text>
       <StatusBar style="auto" />
     </View>
