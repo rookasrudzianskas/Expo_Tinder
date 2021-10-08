3d97ecd4bf7eb53d7fde8a696a2be7d94bce7705
Rokas <rookas.rudzenskas@gmail.com>
2021-10-08 12:49:17 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tinder 🚀</Text>
+    <View style={tw`flex-1`}>
+      <Text style={tw`text-2xl font-bold`}>Tinder 🚀</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
