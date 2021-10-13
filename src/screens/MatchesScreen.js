d9692425b4e38f81e4da8b89e3b2b16955df4d6f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 15:32:07 +0300

    Rokas

--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import tw from "tailwind-react-native-classnames";
 
 const MatchesScreen = () => {
     return (
         <View>
-            <Text>
-                byrookas 🚀
+            <Text style={tw`font-bold text-xl text-pink-400`}>
+                New Matches 🚀
             </Text>
         </View>
     );
