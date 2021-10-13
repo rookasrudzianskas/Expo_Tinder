b8993777c8bd969995226baeaddc7631f2908649
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 12:43:43 +0300

    ROkas

--- App.js
+++ App.js
@@ -98,6 +98,7 @@ const styles = StyleSheet.create({
         height: '70%',
         justifyContent: 'center',
         alignItems: 'center',
+        flex: 1,
     },
     nextCardContainer: {
         ...StyleSheet.absoluteFillObject,
