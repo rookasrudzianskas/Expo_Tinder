83a9a3afdbbdae0a72eaadd08b73fae3fa79ac50
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 09:35:32 +0300

    Rokas

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
-//Just add  in babel.config.js
-
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
+  };
 };
-
-//and close everything and restart the project
