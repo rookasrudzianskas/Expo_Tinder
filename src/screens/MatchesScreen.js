2a7a8edb7e10ab07b240c55134a33e44cb348e24
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 15:26:08 +0300

    Rokas

---
+++ src/screens/MatchesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const MatchesScreen = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default MatchesScreen;
