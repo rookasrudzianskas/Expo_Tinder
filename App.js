4223e40a4caee5a227543d76fcea870b9c933e0e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 15:25:19 +0300

    Rokas

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flexGrow: 1,
+        width: '100%',
     },
 });
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -31,6 +31,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flexGrow: 1,
+        width: '100%',
     },
 });
 
