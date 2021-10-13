a2356c5011f61cdc0c75d41474cc64f856dd4fa0
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 15:02:33 +0300

    Rokas

--- src/components/AnimatedStack/index.js
+++ src/components/AnimatedStack/index.js
@@ -1,14 +1,168 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
+import {
+    Image,
+    ImageBackground,
+    Pressable,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    useWindowDimensions,
+    View
+} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import Animated, {
+    interpolate, runOnJS,
+    useAnimatedGestureHandler,
+    useAnimatedStyle, useDerivedValue,
+    useSharedValue,
+    withSpring
+} from "react-native-reanimated";
+import {PanGestureHandler} from "react-native-gesture-handler";
+import Like from '../../../assets/images/LIKE.png';
+import Nope from '../../../assets/images/nope.png';
+import TinderCard from "../TinderCard";
 
-const AnimatedStack = () => {
+
+const SWIPE_VELOCITY = 800;
+
+
+const AnimatedStack = (props) => {
+
+    const {data} = props;
+    const ROTATION = 60;
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+    const nextProfile = data[nextIndex];
+    const currentProfile = data[currentIndex];
+    const {width: screenWidth} = useWindowDimensions();
+    const hiddenTranslateX = 2 * screenWidth;
+
+    const translateX = useSharedValue(0); /// -width             0            width
+    const rotate = useDerivedValue(() =>
+        interpolate(                          // -60 - left,     0 - center,    60 - right
+            translateX.value,
+            [0, hiddenTranslateX],
+            [0, ROTATION]) + 'deg');
+
+
+    const cardStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value,
+                },
+                {
+                    rotate: rotate.value,
+                }
+            ],
+        };
+    });
+
+    const nextCardStyle = useAnimatedStyle(() => ({
+        transform: [
+            {
+                scale: interpolate(
+                    translateX.value,
+                    [-hiddenTranslateX, 0, hiddenTranslateX],
+                    [1, 0.8, 1],
+                ),
+            },
+        ],
+        opacity: interpolate(
+            translateX.value,
+            [-hiddenTranslateX, 0, hiddenTranslateX],
+            [1, 0.5, 1],
+        ),
+    }));
+
+    const likeStyle = useAnimatedStyle(() => ({
+        opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
+    }));
+
+    const nopeStyle = useAnimatedStyle(() => ({
+        opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
+    }));
+
+    const gestureHandler = useAnimatedGestureHandler({
+        onStart: (_, context) => {
+            context.startX = translateX.value;
+        },
+        onActive: (event, context) => {
+            translateX.value = context.startX + event.translationX;
+        },
+        onEnd: (event) => {
+            if(Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+                translateX.value = withSpring(0);
+                return;
+            }
+
+            // will return 1 if the sign is +, if it is - it will return -1
+            translateX.value = withSpring(hiddenTranslateX * Math.sign(event.velocityX),
+                {},
+                () => runOnJS(setCurrentIndex)(currentIndex + 1),
+            );
+        },
+    });
+
+    useEffect(() => {
+        translateX.value = 0;
+        setNextIndex(currentIndex + 1);
+    }, [currentIndex, translateX]);
+
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+        <View style={styles.pageContainer}>
+            {nextProfile && (
+                <View style={styles.nextCardContainer}>
+                    <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+                        <TinderCard user={nextProfile} />
+                    </Animated.View>
+                </View>
+            )}
+            {currentProfile && (
+                <PanGestureHandler onGestureEvent={gestureHandler} >
+                    <Animated.View style={[styles.animatedCard, cardStyle]}>
+                        <Animated.Image source={Like} style={[styles.like, {left: 10}, likeStyle]} />
+                        <Animated.Image source={Nope} style={[styles.like, {right: 10}, nopeStyle]} />
+                        <TinderCard user={currentProfile} />
+                    </Animated.View>
+                </PanGestureHandler>
+            )}
+
+
+            <StatusBar style="auto" />
+            {/*<TouchableOpacity onPress={() => sharedValue.value = withSpring(Math.random())}>*/}
+            {/*    <Text style={tw``}>Change Value</Text>*/}
+            {/*</TouchableOpacity>*/}
         </View>
     );
-};
+}
+
+const styles = StyleSheet.create({
+    pageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+    },
+    animatedCard: {
+        width: '90%',
+        height: '70%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    nextCardContainer: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    like: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+        position: 'absolute',
+        top: 10,
+        zIndex: 1,
+    }
+});
 
 export default AnimatedStack;
