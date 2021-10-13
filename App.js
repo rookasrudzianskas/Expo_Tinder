b2551056c2b75e4a25729d172269a10bbb06e42f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-13 15:07:41 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
 import {
-    Image,
-    ImageBackground,
-    Pressable,
     StyleSheet,
-    Text,
-    TouchableOpacity,
-    useWindowDimensions,
     View
 } from 'react-native';
-import tw from 'tailwind-react-native-classnames';
 import TinderCard from "./src/components/TinderCard";
 import users from './assets/data/users.js';
-import Animated, {
-    interpolate, runOnJS,
-    useAnimatedGestureHandler,
-    useAnimatedStyle, useDerivedValue,
-    useSharedValue,
-    withSpring
-} from "react-native-reanimated";
-import {PanGestureHandler} from "react-native-gesture-handler";
-import Like from './assets/images/LIKE.png';
-import Nope from './assets/images/nope.png';
 import AnimatedStack from "./src/components/AnimatedStack";
 
 
@@ -43,25 +26,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexGrow: 1,
     },
-    animatedCard: {
-        width: '90%',
-        height: '70%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    nextCardContainer: {
-        ...StyleSheet.absoluteFillObject,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    like: {
-        width: 100,
-        height: 100,
-        resizeMode: 'contain',
-        position: 'absolute',
-        top: 10,
-        zIndex: 1,
-    }
 });
 
 export default App;
