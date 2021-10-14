f943cf3cb8ed6c6a89d14b5358cfdb51fc764163
Rokas <rookas.rudzenskas@gmail.com>
2021-10-14 13:49:33 +0300

    Rokas

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Text, View, StyleSheet, SafeAreaView, Image} from 'react-native';
+import tw from "tailwind-react-native-classnames";
+import users from "../../assets/data/users";
+
+const ProfileScreen = () => {
+    return (
+
+        <SafeAreaView>
+            <View style={{width: '100%', flexGrow: 1, padding: 10}}>
+                <View style={{padding: 10,}}>
+                    <Text style={tw`font-bold text-xl text-pink-400`}>
+                        New Matches 🚀
+                    </Text>
+                    <View style={styles.users}>
+                        {users.map((user, index) => (
+                            <View key={index} style={[styles.user, {marginHorizontal: 10, marginTop: 6}]}>
+                                <Image source={{uri: user.image}} style={styles.image} />
+                            </View>
+                        )) }
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+    user: {
+        width: 100,
+        height: 100,
+        // padding: 8,
+        borderWidth: 2,
+        borderColor: '#F63A6e',
+        borderRadius: 100,
+        padding: 4,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 100,
+    },
+    users: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+
+    },
+});
+
+// backend
