7ad958583b3138f8bf04dc2ead3501bbf17e84ab
Rokas <rookas.rudzenskas@gmail.com>
2021-10-14 14:19:46 +0300

    Rokas

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import {Text, View, StyleSheet, SafeAreaView, Image, TouchableOpacity, TextInput
 import tw from "tailwind-react-native-classnames";
 import users from "../../assets/data/users";
 import {Auth} from 'aws-amplify';
+import {Picker} from '@react-native-picker/picker';
 
 const ProfileScreen = () => {
 
@@ -13,6 +14,7 @@ const ProfileScreen = () => {
 
     const [name, setName] = useState('');
     const [bio, setBio] = useState('');
+    const [selectedLanguage, setSelectedLanguage] = useState();
 
     const save = () => {
 
@@ -31,6 +33,15 @@ const ProfileScreen = () => {
                     <TextInput value={name} style={tw`text-lg`} onChangeText={setName} placeholder={'Enter your name'} />
                     <TextInput style={tw`mt-3 text-lg`} multiline numberOfLines={3} value={bio} onChangeText={setBio} placeholder={'Enter your bio'} />
                 </View>
+
+                <Picker
+                    selectedValue={selectedLanguage}
+                    onValueChange={(itemValue, itemIndex) =>
+                        setSelectedLanguage(itemValue)
+                    }>
+                    <Picker.Item label="Java" value="java" />
+                    <Picker.Item label="JavaScript" value="js" />
+                </Picker>
 
                 <TouchableOpacity onPress={save}>
                     <View style={tw`bg-green-500 px-10 mt-4 py-4 rounded-lg`}>
