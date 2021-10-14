9ebe5580c69b05f9527807a17b0cd804ecd7f2ac
Rokas <rookas.rudzenskas@gmail.com>
2021-10-14 09:45:30 +0300

    Rokas

--- App.js
+++ App.js
@@ -7,16 +7,21 @@ import {
 } from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
 import MatchesScreen from "./src/screens/MatchesScreen";
-import {Fontisto} from "@expo/vector-icons";
+import {FontAwesome, Fontisto, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 
 
 const App = () => {
+
+    const color = '#cbc9c9';
 
   return (
       <SafeAreaView style={{flex: 1,}}>
         <View style={styles.pageContainer}>
             {/*<HomeScreen />*/}
             <Fontisto name="tinder" size={24} color={'#ff0065'} />
+            <MaterialCommunityIcons name="star-four-points" size={24} color={color} />
+            <Ionicons name="ios-chatbubbles" size={24} color={color} />
+            <FontAwesome name="user" size={24} color={color} />
             <MatchesScreen />
           <StatusBar style="auto" />
         </View>
