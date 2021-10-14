f2edf865f8984f35267336448dc04ffd72095adc
Rokas <rookas.rudzenskas@gmail.com>
2021-10-14 09:58:11 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
 import {
-    StyleSheet,
+    StyleSheet, TouchableOpacity,
     View
 } from 'react-native';
 import TinderCard from "./../../src/components/TinderCard";
 import users from '../../assets/data/users.js';
 import AnimatedStack from "./../../src/components/AnimatedStack";
+import {Entypo, FontAwesome, Ionicons} from "@expo/vector-icons";
+import tw from "tailwind-react-native-classnames";
 
 
 const HomeScreen = () => {
@@ -22,6 +24,33 @@ const HomeScreen = () => {
     return (
         <View style={styles.pageContainer}>
             <AnimatedStack onSwipeLeft={onSwipeLeft} onSwipeRight={onSwipeRight} data={users} renderItem={({item}) => <TinderCard user={item} />} />
+            <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around', marginHorizontal: 10,}}>
+                <TouchableOpacity>
+                    <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
+                    <FontAwesome name="undo" size={30} color="#FBD88B" />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
+                    <Entypo name="cross" size={30} color="#F76C6B" />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
+                    <FontAwesome name="star" size={30} color="#3AB4CC" />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
+                    <FontAwesome name="heart" size={30} color="#4FCC94" />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
+                    <Ionicons name="flash" size={30} color="#A65CD2" />
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -32,6 +61,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexGrow: 1,
         width: '100%',
+        // backgroundColor: '#ededed',
     },
 });
 
