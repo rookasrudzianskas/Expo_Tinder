f8d62de671e82d3dd4717abb098b850039fa0f96
Rokas <rookas.rudzenskas@gmail.com>
2021-10-14 10:35:42 +0300

    ROkas

--- src/screens/SecondPartOfHomeScreen.js
+++ src/screens/SecondPartOfHomeScreen.js
@@ -5,24 +5,31 @@ import {
     StyleSheet, Text, TouchableOpacity,
     View
 } from 'react-native';
-import HomeScreen from "./src/screens/HomeScreen";
-import MatchesScreen from "./src/screens/MatchesScreen";
+import HomeScreen from "../../src/screens/HomeScreen";
+import MatchesScreen from "../../src/screens/MatchesScreen";
 import {FontAwesome, Fontisto, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 import tw from "tailwind-react-native-classnames";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import {useNavigation} from "@react-navigation/native";
 
 
-const App = () => {
+const SecondPartOfHomeScreen = () => {
 
     const color = '#b5b5b5';
     const activeColor = '#ff0065';
     const [activeScreen, setActiveScreen] = useState('HOME');
+    const navigation = useNavigation();
+
+    const goToHome = () => {
+        setActiveScreen('HOME');
+        navigation.navigate('Home');
+    }
 
     return (
         <SafeAreaView style={{flex: 1,}}>
             <View style={styles.pageContainer}>
                 <View style={[tw`flex flex-row mt-6`, {justifyContent: 'space-around', width: '100%'}]}>
-                    <TouchableOpacity onPress={() => setActiveScreen('HOME')}>
+                    <TouchableOpacity onPress={goToHome}>
                         <Fontisto name="tinder" size={30} color={activeScreen === 'HOME' ? activeColor  : color} />
                     </TouchableOpacity>
                     <MaterialCommunityIcons name="star-four-points" size={30} color={color} />
@@ -35,7 +42,6 @@ const App = () => {
                 <HomeScreen />
 
                 {/*<MatchesScreen />*/}
-                <StatusBar style="auto" />
             </View>
         </SafeAreaView>
     );
@@ -49,4 +55,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default App;
+export default SecondPartOfHomeScreen;
