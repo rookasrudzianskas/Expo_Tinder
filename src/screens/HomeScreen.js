a2c4fe3aed6af4b686801f6b927e1f57305569b7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-17 11:24:35 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -90,7 +90,11 @@ const HomeScreen = () => {
 
     return (
         <View style={styles.pageContainer}>
-            {users.length > 0  && <AnimatedStack setCurrentUser={setCurrentUser} onSwipeLeft={onSwipeLeft} onSwipeRight={onSwipeRight} data={users} renderItem={({item}) => <TinderCard user={item} />} />}
+            {users.length > 0  ? (
+                <AnimatedStack setCurrentUser={setCurrentUser} onSwipeLeft={onSwipeLeft} onSwipeRight={onSwipeRight} data={users} renderItem={({item}) => <TinderCard user={item} />} />
+            ): (
+                <Text>Rokas</Text>
+            )}
             <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around', marginHorizontal: 10, marginTop: -80}}>
                 <TouchableOpacity>
                     <View style={tw`bg-gray-100 p-5 rounded-full shadow-md`}>
